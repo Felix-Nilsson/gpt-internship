5e6e8766e5df916c925ce3c24b7f0ad870169e8b
Felix Nilsson <felix@pop-os.localdomain>
2023-06-19 14:31:11 +0200

    worked on long term conversation memory

--- embeddings/chatbot.py
+++ embeddings/chatbot.py
@@ -6,24 +6,29 @@ from run_query import return_best_record
 #with open('journal.txt', 'r') as file:
 #    journal = file.read().replace('\n', '')
 
-
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-def get_completion(prompt, model="gpt-3.5-turbo"): # Andrew mentioned that the prompt/ completion paradigm is preferable for this class
-    messages = [{'role':'system', 'content':
+conversation = [{'role':'system', 'content':
                     'Du är en assistent för sjukvårdspersonal som hjälper dem med deras förberedelser \
                     inför möte med en patient. Använd patientens information för att svara på frågorna. \
                     Om patientens information är på engelska, översätt den till svenska och använd det \
                     för att svara. Om du inte kan svara på en fråga utifrån den information som \
                     finns, svara att du inte vet. Svara alltid på svenska.'},
-        {"role": "user", "content": prompt}]
+        ]
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def get_completion(prompt, model="gpt-3.5-turbo",): # Andrew mentioned that the prompt/ completion paradigm is preferable for this class
+    conversation.append({'role':'user','content':prompt})
+    messages = conversation
+
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
         temperature=0, # this is the degree of randomness of the model's output
     )
     #print(response)
+    conversation.append({'role':'assistant','content':response})
+    print(conversation)
     return response.choices[0].message["content"]
 
 def get_chat_response(query):
@@ -41,3 +46,5 @@ def get_chat_response(query):
 
     return response
 
+
+#get_chat_response("Vilken patient är rädd för cyklar?")

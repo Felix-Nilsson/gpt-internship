8c139720c0508a83f68ec03fbcc687fc0f2e9865
Felix Nilsson <felix@pop-os.localdomain>
2023-06-20 08:27:09 +0200

    removed duplicate files

--- misc/test.py
+++
@@ -1,7 +0,0 @@
-import os
-import openai
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
-print(chat_completion)

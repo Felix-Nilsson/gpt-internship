761d1cf71f59e865dc233ce46dd01a30045084b5
Felix Nilsson <felix@pop-os.localdomain>
2023-06-26 08:31:10 +0200

    Now correctly reads json files, currently 2 at a time

---
+++ Project1_docassist/embeddings/fileparser.py
@@ -0,0 +1,46 @@
+from PyPDF2 import PdfReader
+import json
+
+def pdf_to_plaintext(filename):
+    reader = PdfReader(filename)
+
+    num_pages = len(reader.pages)
+    text = ""
+    for i in range(num_pages):
+        page = reader.pages[i]
+        text += page.extract_text().replace(" ", "").replace("\n"," ")
+    
+    return text
+
+def read_records():
+    texts = []
+    for i in range(4):
+        adr = f'Project1_docassist/patientrecords/patientrecord_{i}.pdf'
+        text = pdf_to_plaintext(adr)
+        texts.append(text)
+    return texts
+
+#todo - make this part of read records
+def read_json_records():
+    texts = []
+    for i in range(6):
+        adr = f'Project1_docassist/patientrecords/patientdata_{i}.json'
+        text =read_json(adr)
+        texts.append(text)
+    return texts
+
+
+def read_json(filename):
+    with open(filename) as f:
+        data = json.load(f)
+        #data = data['patient']
+        #data = f.read()
+        return str(data)
+
+
+#print(read_json('Project1_docassist/patientrecords/patientdata_0.json'))
+
+with open("Project1_docassist/patientrecords/patientdata_0.json", "r") as f:
+    parsed = json.load(f)
+    print("Printed patientdata_0.json from fileparser.py")
+    print(json.dumps(parsed, indent=4))

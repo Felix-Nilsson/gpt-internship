04ead18c17d5e3b715b87521cef5d9ef746802e6
Felix Nilsson <felix@pop-os.localdomain>
2023-06-27 16:56:38 +0200

    now saves test results

--- Project1_docassist/test_embeddings.py
+++ Project1_docassist/test_embeddings.py
@@ -1,6 +1,7 @@
 from embeddings.make_embeddings import make_embeddings
 from embeddings.chatbot import get_chat_response
 from embeddings.run_query import strings_ranked_by_relatedness
+import numpy
 
 import json
 
@@ -12,13 +13,21 @@ def run_test(question,reference_answers,identifier):
     embedded_references = make_embeddings(reference_answers)
 
     return strings_ranked_by_relatedness(candidate,embedded_references)[1]
+with open("Project1_docassist/tests/results.csv","w") as g:
+    g.write("max,min,avg")
 
-with open("Project1_docassist/tests/cases.json") as f:
-    data = json.load(f)
-    for case in data:
-        question = case['question']
-        reference_answers = case['reference_answers']
-        identifier = case['patient_id']
+    with open("Project1_docassist/tests/cases.json","r") as f:
+        data = json.load(f)
+        for case in data:
+            question = case['question']
+            reference_answers = case['reference_answers']
+            identifier = case['patient_id']
 
-        results = run_test(question,reference_answers,identifier)
-        print(results)
+            results = run_test(question,reference_answers,identifier)
+            low = min(results)
+            high = max(results)
+            mean = numpy.mean(results)
+
+            g.write(f"\n{high},{low},{mean}")
+
+            

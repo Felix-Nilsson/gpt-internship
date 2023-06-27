db9d88c0bad6b2448700383d7283ab025cf3c91d
OGPauli <oskar.pauli@bredband.net>
2023-06-27 17:04:57 +0200

    Added bleu tester

--- Project1_docassist/bleu.py
+++ Project1_docassist/bleu.py
@@ -1,16 +1,27 @@
 from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 from embeddings.chatbot import get_chat_response
-
-query = "Har patient 654321 fått hjärtinfarkt?"
-ref = ["Ja, han har fått akut transmural hjärtinfarkt"]#["Han fick transmural hjärtinfarkt, så det stämmer"]]
-can = [get_chat_response(query,["654321"])]
-can = can[0].split("*")[0].replace("\n","").strip()
+import json
 
 
+def run_bleu(question, reference_answers, identifier):
+    can = [get_chat_response(question,[identifier])]
+    can = can[0].split("*")[0].replace("\n","").strip()
+    can = can.split()
+    return sentence_bleu(reference_answers, can)
+    
 
-#references = [['This is reference sentence 1.'], ['This is reference sentence 2.']]
-#candidates = ['This is candidate sentence 1.', 'This is candidate sentence 2.']
-print(can)
-# Calculate BLEU score
-score = sentence_bleu([ref], can)
-print("BLEU score:", score)
+with open('Project1_docassist/tests/cases.json') as file:
+    data = json.load(file)
+    
+    for case in data:
+        question = case['question']
+        reference_answers = case['reference_answers']
+        reference_answers = [answer.split() for answer in reference_answers]
+        identifier = case['patient_id']
+
+        results = run_bleu(question,reference_answers,identifier)
+        print(results)
+
+
+
+

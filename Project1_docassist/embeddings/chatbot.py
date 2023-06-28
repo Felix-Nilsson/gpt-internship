91aa516da10966d9921c44a96fac6cc8dfe2d7d5
Henrik <hejo@student.chalmers.se>
2023-06-28 13:44:35 +0200

    Cleanup and documentation

--- Project1_docassist/embeddings/chatbot.py
+++ Project1_docassist/embeddings/chatbot.py
@@ -4,38 +4,43 @@ from embeddings.run_query import return_best_record
 import json
 import re
 
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# ! Do not forget to set the environment variable !
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
+#All patients that have been asked about
 patient_ids = []
 
-conversation = [{'role':'system', 'content': ""}]
+#AI's memory of the conversation
+messages = [{'role':'system', 'content': ""}]
 
 
-def find_patient_ids(string):
+def find_patient_id(text: str):
+    """"Searches through text for a 6-digit patient ID"""
+
     pattern = r'\d{6}'  # Regular expression pattern to match six digits in a row
-    match = re.search(pattern, string)
+    match = re.search(pattern, text)
     if match:
         return match.group()
     else:
-        return "NONE"
+        return None
 
-def get_chat_response(query, patients, model="gpt-3.5-turbo-0613"):
+def get_chat_response(query: str, patients: list[str], model='gpt-3.5-turbo-0613'):
+    """Takes a query and a list of patients whose information the doctor can access, returns a response to the query"""
     
-    response_message = find_patient_ids(query)
+    current_patient_id = find_patient_id(query)
 
-    if response_message != "NONE" and response_message in patients:
-        patient_ids.insert(0,response_message)
+    #Check if the patient 'belongs' to the current doctor
+    if (current_patient_id is not None) and (current_patient_id in patients):
+        patient_ids.insert(0,current_patient_id)
 
-    print("patient_ids:",patient_ids)
 
-    #CALL GPT WITH THE QUERY AND RELEVANT PATIENT INFORMATION
+    #Get patient data related to the query
     patient_data = ""
     if patient_ids != []:
         patient_data = return_best_record(query, patient_ids[0])
         patient_data = " ".join(patient_data[0])
 
-    #context has to be defined here now since the patient_data is added into it rather than the prompt
+    #Context/System message to describe what the gpt is supposed to do
     context_with_data = f'''
     Du är en AI-assistent för läkare på ett sjukhus.
     Du svarar alltid kort och koncist, inte längre än 2 meningar.
@@ -45,23 +50,24 @@ def get_chat_response(query, patients, model="gpt-3.5-turbo-0613"):
     ___{patient_data}___
     '''
 
-    messages = conversation
-
+    #Add the query to the AI's memory
     messages.append({'role':'user','content':query})
 
     #Update the context with relevant information for every question
     messages[0] = {'role':'system', 'content': context_with_data}
 
 
-    second_response = openai.ChatCompletion.create(
+    response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
         temperature=0, # this is the degree of randomness of the model's output
     )
 
-    messages.append({'role':'assistant','content':second_response.choices[0].message["content"]})
+    #Add the response to the AI's memory
+    messages.append({'role':'assistant','content':response.choices[0].message["content"]})
 
-    finished_response = f'''{second_response.choices[0].message["content"]} 
+
+    finished_response = f'''{response.choices[0].message["content"]} 
     \n*OBS: som läkare bär du alltid själv ansvaret mot patienten*'''
 
     return(finished_response)

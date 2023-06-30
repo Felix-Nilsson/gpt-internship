ed0e6cb5bd79410652a79ca6a2b22a1cd463b653
Henrik <hejo@student.chalmers.se>
2023-06-30 09:04:47 +0200

    Fixed imports for test_embeddings

--- Project_assistant/testing/test_embeddings.py
+++ Project_assistant/testing/test_embeddings.py
@@ -1,13 +1,18 @@
+#Fix the imports of files in the embeddings folder
+import sys
+sys.path.append("c:\\Users\\henke\\Documents\\Git\\gpt-internship\\Project_assistant")
+
+import embeddings.chatbot as chatbot
 from embeddings.make_embeddings import make_embeddings
-from embeddings.chatbot import get_chat_response
 from embeddings.run_query import strings_ranked_by_relatedness
 import numpy as np
 
 import json
 
 def run_similarity_test(question,reference_answers,identifier):
+    cb = chatbot.Chatbot('Doctor')
 
-    candidate = [get_chat_response(question,[str(identifier)])]
+    candidate = [cb.get_chat_response(question,[str(identifier)])]
     candidate = candidate[0].split("*")[0].replace("\n","").strip()
 
     embedded_references = make_embeddings(reference_answers)
@@ -20,10 +25,10 @@ def run_similarity_test(question,reference_answers,identifier):
 
 
 def run_positive_tests():
-    with open("testing/tests/data/positive_results.csv","w") as g:
+    with open("Project_assistant/testing/tests/data/positive_results.csv","w", encoding="utf-8") as g:
         g.write("index,max,min,avg")
 
-        with open("testing/tests/cases.json","r") as f:
+        with open("Project_assistant/testing/tests/cases.json","r", encoding="utf-8") as f:
             data = json.load(f)
             for case in data:
                 index = case["test_index"]
@@ -40,10 +45,10 @@ def run_positive_tests():
                 g.write(f"\n{index},{high},{low},{mean}")
 
 def run_negative_tests():
-    with open("testing/tests/data/negative_results.csv","w") as g:
+    with open("Project_assistant/testing/tests/data/negative_results.csv","w", encoding="utf-8") as g:
         g.write("index,max,min,avg")
 
-        with open("testing/tests/cases.json","r") as f:
+        with open("Project_assistant/testing/tests/cases.json","r", encoding="utf-8") as f:
             data = json.load(f)
             for case in data:
                 index = case["test_index"]

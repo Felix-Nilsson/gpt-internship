db95b26de5320ef44250b6325d27c01a6d333894
OGPauli <oskar.pauli@bredband.net>
2023-07-04 09:57:00 +0200

    Added duckduckgo-search functionality

--- Project4_internet/src/test_agent.py
+++ Project4_internet/src/test_agent.py
@@ -1,16 +1,26 @@
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
+from langchain.agents import Tool
 from langchain.agents import AgentType
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI 
+from langchain.agents import initialize_agent
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.prompts import ChatPromptTemplate
 import os
 
 OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
 llm = OpenAI(temperature=0)
 
-tools = load_tools(["llm-math"], llm=llm)
+def search(query):
+    search = DuckDuckGoSearchRun()
+    return search.run(f"{query} site:1177.se")
 
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+tool = [Tool(name='wiki', func=search, description='Useful when you want information, current or old.')]
 
-agent.run("My brother is 20 years old? What is his current age raised to the 0.43 power?")
+agent = initialize_agent(tool, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, early_stopping_method="generate")
+
+agent.run("Jag har ont i huvudet, vad borde jag göra?")
+
+
+
+
 

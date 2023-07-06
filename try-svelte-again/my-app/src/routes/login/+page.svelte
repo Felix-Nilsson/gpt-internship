a002a155dc6bcf4f440ec6e2436083cffef8c0e5
Felix Nilsson <felix@pop-os.localdomain>
2023-07-06 09:59:44 +0200

    Added login page and changed first page to start page

--- try-svelte-again/my-app/src/routes/login/+page.svelte
+++ try-svelte-again/my-app/src/routes/login/+page.svelte
@@ -1,20 +1,33 @@
 <script>
-    import { Button, Group, Title } from '@svelteuidev/core';
-</script>
+    import { Input, InputWrapper, Title, Group, Button } from '@svelteuidev/core';
+    import { EnvelopeClosed,LockClosed } from 'radix-icons-svelte';
+  </script>
+  
+  
 
-<div class="center-screen">
+   <div class="center-screen">
     <Group spacing="lg" direction="column">
         <Title order={1} variant='gradient' gradient={{from: 'blue', to: 'red', deg: 45}} style="font-size: 5rem">
             MedHelp
         </Title>
-            <Button variant='gradient' gradient={{from: 'teal', to: 'blue', deg: 60}} radius="lg" size="xl" ripple>
-                Logga in
-            </Button>
-    
-            <Button variant="outline" radius="lg" size="xl" ripple>
-                Internethjälp
-            </Button>
         
+        <Input icon={EnvelopeClosed} 
+            placeholder="Personnummer"
+            radius="lg"
+            size="xl"
+        />
+
+        <Input 
+            type="password" 
+            icon={LockClosed} 
+            placeholder="Lösenord"
+            radius="lg"
+            size="xl"
+        />
+
+        <Button variant='gradient' gradient={{from: 'teal', to: 'blue', deg: 60}} radius="lg" size="xl" ripple>
+            Skicka
+        </Button>
     </Group>
 </div>
 

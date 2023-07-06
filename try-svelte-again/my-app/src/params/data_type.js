7c7808fc30a16a36ff00d694287d94a0e3c2c24a
Henrik <hejo@student.chalmers.se>
2023-07-06 11:33:56 +0200

    removed unneeded files/folders

--- try-svelte-again/my-app/src/params/data_type.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@sveltejs/kit').ParamMatcher} */
-export function match(param) {
-    if (param == 'input' || param == 'response')
-        return true;
-    //return /^\d+$/.test(param);
-}

91a216f8eee5eb3923f5ccd75a641bccf2b2fafb
Felix Nilsson <felix@pop-os.localdomain>
2023-07-06 10:45:32 +0200

    started work on chat

---
+++ try-svelte-again/my-app/src/routes/chat/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { Text, Stack, Group, Title } from '@svelteuidev/core';
+</script>
+
+
+
+<div class="center-screen">
+    <Group spacing="lg" direction="column">
+        <Title order={1} variant='gradient' gradient={{from: 'blue', to: 'red', deg: 45}} style="font-size: 5rem">
+            MedHelp
+        </Title>
+
+        <Stack override={{ height: 300 }}  align="flex-start" spacing="lg">
+            <Text
+                component='span'
+                align='center'
+                variant='gradient'
+                size='xl'
+                weight='bold'
+            >
+                Text with gradient
+            </Text>
+
+            <Text
+            component='span'
+            align='center'
+            variant='gradient'
+            size='xl'
+            weight='bold'
+        >
+            Text with gradient
+        </Text>
+
+        </Stack>
+
+    </Group>
+    <Text
+    component='span'
+    align='center'
+    variant='gradient'
+    size='xl'
+    weight='bold'
+    gradient={{ from: 'red', to: 'yellow' }} 
+>
+    Text with gradient
+</Text>
+</div>
+

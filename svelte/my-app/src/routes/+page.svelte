50d248d1bb476b12e9dd252d98156eb4e6e076d9
Henrik <hejo@student.chalmers.se>
2023-07-07 15:45:13 +0200

    GIF?

--- svelte/my-app/src/routes/+page.svelte
+++ svelte/my-app/src/routes/+page.svelte
@@ -2,7 +2,11 @@
     import { Button, Group, Title } from '@svelteuidev/core';
 </script>
 
+
 <div class="center-screen">
+    <!--<iframe 
+        title='' src="https://gifer.com/embed/7fpZ" width="100%" height="100%" style='position:absolute;top:0;left:0;'>
+    </iframe>-->
     <Group spacing="lg" direction="column">
         <Title order={1} variant='gradient' gradient={{from: 'blue', to: 'red', deg: 45}} style="font-size: 5rem; line-height: 1.5">
             Sahlgrenska AI Hjälp

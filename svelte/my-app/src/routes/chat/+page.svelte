87c93c0fea16a41d5f8163f690c3da45bb952d95
Henrik <hejo@student.chalmers.se>
2023-07-20 09:45:49 +0200

    :sparkles: Start of settings, only button

--- svelte/my-app/src/routes/chat/+page.svelte
+++ svelte/my-app/src/routes/chat/+page.svelte
@@ -1,11 +1,13 @@
 <script>
-    import { Group, Title, Input , Button, Center, Burger, Stack, Text, Space, Divider } from '@svelteuidev/core';
+    import { Group, Title, Input , Button, Center, Burger, Stack, Text, Space, Divider, Flex } from '@svelteuidev/core';
     import Conversation from "./Conversation.svelte";
     import { onMount } from 'svelte';
     import { goto } from '$app/navigation'
+    import Icon from '@iconify/svelte';
 
 
     let opened = false;
+    let show_settings = false;
     let input = "";
 
     let current_credentials = {'username': ''};
@@ -191,6 +193,32 @@
 </div>
 
 
+<!--Settings menu-->
+<Center>
+    <div class="settings">
+        <Center>
+            <Button variant="subtle" size="xl" compact ripple>
+                <Flex justify="center" direction="column" override={{ gap: 0 }}>
+                    <Center>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3.13523 8.84197C3.3241 9.04343 3.64052 9.05363 3.84197 8.86477L7.5 5.43536L11.158 8.86477C11.3595 9.05363 11.6759 9.04343 11.8648 8.84197C12.0536 8.64051 12.0434 8.32409 11.842 8.13523L7.84197 4.38523C7.64964 4.20492 7.35036 4.20492 7.15803 4.38523L3.15803 8.13523C2.95657 8.32409 2.94637 8.64051 3.13523 8.84197Z" fill="Black" fill-rule="evenodd" clip-rule="evenodd"></path>
+                        </svg>
+                    </Center>
+                    <Center>
+                        <Text size="sm">
+                            Inställningar
+                        </Text>
+                    </Center>
+                </Flex>
+            </Button>
+        </Center>
+    </div>
+</Center>
+
+
+
+
+
 <!--Header-->
 <div class="header">
 
@@ -255,6 +283,15 @@
 
 
 <style>
+
+    .settings {
+        background: transparent; 
+        position: absolute; 
+        margin: 85px; 
+        bottom: 0; 
+        max-height:800px; 
+        width: 600px;
+    }
     
     .header {
         position: fixed;
@@ -304,6 +341,7 @@
         bottom: 80px;
         width: 200px;
     }
+
 
 
 </style>

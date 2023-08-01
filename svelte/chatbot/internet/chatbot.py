53a67c5996c62f09f1a433b2c26cf5be64511cfa
Henrik <hejo@student.chalmers.se>
2023-08-01 13:17:28 +0200

    :hammer: Small changes in internet chatbot
    But it still completely ignores the language level setting
    
    :sparkles: Added an explanation that shows the settings for the message.

--- svelte/chatbot/internet/chatbot.py
+++ svelte/chatbot/internet/chatbot.py
@@ -24,7 +24,7 @@ class Chatbot:
             return_messages=True
         )
 
-    def get_chat_response(self, query, settings):
+    def get_chat_response(self, query, settings={}):
         """Takes a query and a dictionary containing relevant settings, returns a Message containing all relevant information.
         
         :param query: The query/prompt.
@@ -34,14 +34,14 @@ class Chatbot:
         """
 
         #Adapt language level from settings
-        language_level_sys_message = 'Assistent anpassar alltid sitt svar utefter mottagaren. Mottagare: '
+        #language_level_sys_message = 'Assistent svarar alltid så att ett barn ska kunna förstå'
 
         if settings['language_level'] == 'easy':
-            language_level_sys_message += '10-åring'
+            language_level_sys_message = 'Du svarar alltid så att ett barn ska kunna förstå, med en snäll ton.'
         elif settings['language_level'] == 'complex':
-            language_level_sys_message += 'Läkare'
-        else: #Anything other than easy or complex => normal
-            language_level_sys_message += 'Vuxen med svenska som modersmål'
+            language_level_sys_message = 'Du svarar alltid kort och koncist med en formell stil.'
+        else: #Anything other than easy or complex => normal (the default)
+            language_level_sys_message = 'Du svarar alltid med en trevlig ton och förtydligar allt så att en person som är opåläst om sjukvård ska kunna förstå.'
 
         
         #Change accessible tools from settings
@@ -53,6 +53,8 @@ class Chatbot:
             tools.append(ToolFASS())
         if "internetmedicin.se" in settings['chosen_tools']:
             tools.append(ToolInternetmedicin())
+
+        explanation = f'''Enligt inställningarna ska assistenten svara med språknivå "{settings['language_level']}" och har tillgång till källorna {settings['chosen_tools']}'''
 
         agent = initialize_agent(
             tools=tools, 
@@ -66,13 +68,11 @@ class Chatbot:
         )
 
         #Update the context/system message
-        system_message = f"""Assistent är en 'large language model' skapad för att hjälpa läkare och patienter att hitta information.
-        
-        Assistent svarar alltid på svenska, oavsett vilket språk användaren använder.
+        system_message = f"""
+        Du är en internetassistent.
 
-        
-        Assistent vet ingenting själv och använder därför alltid bara de verktyg som Assistent har tillgång till.
-        Ifall Assistent inte skulle ha tillgång till några verktyg alls, ber Assistent användaren att dubbelkolla inställningarna så att det finns verktyg för Assistent att använda.
+        Du kan ingenting själv utan använder alltid dina verktyg (Tools) för att hitta information som du använder för att svara på frågor.
+        Ifall du inte har tillgång till några verktyg (Tools) be användaren att dubbelkolla inställningarna.
 
         {language_level_sys_message}
         """
@@ -94,4 +94,22 @@ class Chatbot:
         if len(output['intermediate_steps']) != 0:
             sources = output['intermediate_steps'][0]
 
-        return Message(user=False, content=response, sources=sources)
+        return Message(user=False, content=response, sources=sources, explanation=explanation)
+    
+
+
+old_language_level_code = """if settings['language_level'] == 'easy':
+            language_level_sys_message = 'Assistent svarar alltid med snäll ton och enkelt språk, så att ett barn ska kunna förstå.'
+elif settings['language_level'] == 'complex':
+    language_level_sys_message = 'Assistent svarar alltid kort och koncist med en formell stil.'
+else: #Anything other than easy or complex => normal
+    language_level_sys_message = 'Assistent svarar alltid med en trevlig ton och förtydligar allt så att en person som är opåläst om sjukvård ska kunna förstå.'"""
+
+old_system_message = f"""Assistent är en 'large language model' skapad för att hjälpa läkare och patienter att hitta information.
+        
+Assistent svarar alltid på svenska, oavsett vilket språk användaren använder.
+
+Assistent vet ingenting själv och använder därför alltid bara de verktyg som Assistent har tillgång till.
+Ifall Assistent inte skulle ha tillgång till några verktyg alls, ber Assistent användaren att dubbelkolla inställningarna så att det finns verktyg för Assistent att använda.
+
+"""#{language_level_sys_message}

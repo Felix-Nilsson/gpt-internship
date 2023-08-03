03c8e0a1a68c3d3be9bc0b1de01daa223b80b008
OGPauli <oskar.pauli@bredband.net>
2023-08-03 14:17:43 +0200

    :hammer: kept working on unified palce to store prompts

--- prompts/preprocess.py
+++ prompts/preprocess.py
@@ -4,18 +4,38 @@ prompt_doctor = f"""
     input_msg
 
     Ifall meddelandet ber om information om en specifik patient, använd informationen avgränsad av tre understreck.
-    Ifall det inte finns någon information avgränsad av tre understreck, Svara med "Jag har inte tillräckligt med information":
+    Ifall det inte finns någon information avgränsad av två måsvingar, Svara med "Jag har inte tillräckligt med information":
 
-    """
+    """ + "{{background}}"
+
+prompt_patient = f"""
+    Du är en AI-assistent för en patient på ett sjukhus.
+    Du svarar alltid kort och koncist, inte längre än 2 meningar.
+    input_msg
+
+    Ifall meddelandet ber om information om en specifik patient, använd informationen avgränsad av tre understreck.
+    Ifall det inte finns någon information avgränsad av två måsvingar, Svara med "Jag har inte tillräckligt med information":
+
+    """ + "{{background}}"
 
 
-def export_test():
+def export_doc_test():
     global prompt_doctor
     input_msg = 'Du ska svara på följande meddelande "{{input}}".'
-    prompt_doctor.replace("inpu_msg",input_msg)
-    prompt_doctor += "{{background}}"
+    prompt_doctor.replace("input_msg",input_msg)
 
-    with open("prompts/prompt_doctor_cb.txt","w") as f:
+    with open("prompts/prompt_doctor_test.txt","w") as f:
         f.write(prompt_doctor)
 
-export_test()
+def export_pat_test():
+    global prompt_patient
+    input_msg = 'Du ska svara på följande meddelande "{{input}}".'
+    prompt_patient.replace("input_msg",input_msg)
+
+    with open("prompts/prompt_patient_test.txt","w") as f:
+        f.write(prompt_patient)
+
+
+# export_doc_test()
+
+export_pat_test()

9a973ef4acb06886922be774e54c9bc023920069
OGPauli <oskar.pauli@bredband.net>
2023-08-03 14:24:00 +0200

    :hammer: added intranet prompt to global storage
    Co-Authored-By: Felix Nilsson <54684510+Felix-Nilsson@users.noreply.github.com>
    Co-Authored-By: Henrik <54803889+henkejson@users.noreply.github.com>

--- prompts/preprocess.py
+++ prompts/preprocess.py
@@ -18,13 +18,26 @@ prompt_patient = f"""
 
     """ + "{{background}}"
 
+prompt_intranet = f'''
+    Du är en AI-assistent som ska svara på frågor.
+    Du svarar alltid kortare än 2 meningar.
+    input_msg
+
+    Säg gärna vilken fil du hittade informationen i.
+    Du får endast använda informationen som är avgränsad med tre understreck.
+    Använd bara informationen som är avgränsad med två måsvingar.
+    Om du inte hittar svaret i informationen svarar du att du inte har tillgång till informationen.
+
+    
+    ''' + "{{background}}"
+
 
 def export_doc_test():
     global prompt_doctor
     input_msg = 'Du ska svara på följande meddelande "{{input}}".'
     prompt_doctor.replace("input_msg",input_msg)
 
-    with open("prompts/prompt_doctor_test.txt","w") as f:
+    with open("prompts/prompt_doctor_test.txt","w",encoding='utf-8') as f:
         f.write(prompt_doctor)
 
 def export_pat_test():
@@ -32,10 +45,17 @@ def export_pat_test():
     input_msg = 'Du ska svara på följande meddelande "{{input}}".'
     prompt_patient.replace("input_msg",input_msg)
 
-    with open("prompts/prompt_patient_test.txt","w") as f:
+    with open("prompts/prompt_patient_test.txt","w",encoding='utf-8') as f:
         f.write(prompt_patient)
 
+def export_intranet_test():
+    global prompt_intranet
+    input_msg = 'Du ska svara på följande meddelande "{{input}}".'
+    prompt_intranet.replace("input_msg",input_msg)
+
+    with open("prompts/prompt_intranet_test.txt","w",encoding='utf-8') as f:
+        f.write(prompt_intranet)
 
 # export_doc_test()
-
-export_pat_test()
+# export_pat_test()
+export_intranet_test()

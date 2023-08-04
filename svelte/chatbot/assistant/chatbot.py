0d9b8158526b91654fbccd3424da2fe503c3f29b
Henrik <hejo@student.chalmers.se>
2023-08-04 11:50:55 +0200

    :sparkles: :hammer: Doctor/Patient bot gets sys_msg from .txt

--- svelte/chatbot/assistant/chatbot.py
+++ svelte/chatbot/assistant/chatbot.py
@@ -35,10 +35,39 @@ class Chatbot:
             return None
 
 
+    def get_system_message(self):
+        # Path to the system message/system prompt file
+        current_script_path = os.path.abspath(__file__)
+        parent_directory = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(current_script_path))))
+        
+        # Read the correct file
+        if self.user_type == 'doctor':
+            file_path = os.path.join(parent_directory, 'prompts', 'prompts', 'prompt_doctor_test.txt')
+        elif self.user_type == 'patient':
+            file_path = os.path.join(parent_directory, 'prompts', 'prompts', 'prompt_patient_test.txt')
+
+        # Get the system message from the file
+        system_message = ""
+        with open(file_path, "r", encoding='utf-8') as f:
+            system_message = f.read()
+        
+        return system_message
+
+
+    def get_settings_system_message(self, settings):
+        # Adapt language level from settings
+        if settings['language_level'] == 'easy':
+            return 'Du svarar alltid så att ett barn ska kunna förstå, med en snäll ton.'
+        elif settings['language_level'] == 'complex':
+            return 'Du svarar alltid kort och koncist med en formell stil.'
+        else: #Anything other than easy or complex => normal (the default)
+            return 'Du svarar alltid koncist och tydligt, med en konversionell ton'
+
 
     def get_chat_response(self,query: str, settings: dict, patients: list[str], remember=True, model='gpt-3.5-turbo-0613'):
         """Takes a query and a list of patients whose information the doctor can access, returns a Message containing all relevant information.
-        
+        (Doctor / Patient)
+
         :param query: The query/prompt.
         :param patients: If doctor list of accessible patients, if patient list of only patient ID.
         :param remember: If the bot should remember the conversation.
@@ -46,43 +75,18 @@ class Chatbot:
         :return: Message with all needed information, check message.py in utils for more information.
         """
 
-        #The return values
+        # The return values
         final_response = ''
         current_patient = None
         source = None
         explanation = None
         alert_message = None
 
-        #Adapt language level from settings
-        if settings['language_level'] == 'easy':
-            language_level_sys_message = 'Du svarar alltid så att ett barn ska kunna förstå, med en snäll ton.'
-        elif settings['language_level'] == 'complex':
-            language_level_sys_message = 'Du svarar alltid kort och koncist med en formell stil.'
-        else: #Anything other than easy or complex => normal (the default)
-            language_level_sys_message = 'Du svarar alltid med en trevlig ton och förtydligar allt så att en person som är opåläst om sjukvård ska kunna förstå.'
 
-        #System messages to describe what the gpt is supposed to do
-        sys_message_doctor = f'''
-        Du är en AI-assistent för läkare på ett sjukhus.
-        Du svarar alltid kort och koncist, inte längre än 2 meningar.
-        {language_level_sys_message}
-        
-        Ifall meddelandet ber om information om en specifik patient, använd informationen avgränsad av tre understreck.
-        Ifall det inte finns någon information avgränsad av tre understreck, Svara med "Jag har inte tillräckligt med information"
-        '''
-
-        sys_message_patient = f'''
-        Du är en AI-assistent för en patient på ett sjukhus.
-        Du svarar alltid kort och koncist, inte längre än 2 meningar.
-        {language_level_sys_message}
-        Du får använda informationen avgränsad av tre understreck.
-        '''
-        
         if self.user_type == 'doctor':
             #Find patient ID in the query
             current_patient = self.find_patient_id(query)
         
-
             if current_patient == None:
 
                 if self.patient_ids == []: #No ID provided or in memory
@@ -104,7 +108,6 @@ class Chatbot:
                 source = 'Inga källor har använts'
                 explanation = 'Du gav ett ID men du har inte tillgång till den patientens information, därför kan inte assistenten svara.'
 
-
         elif self.user_type == 'patient':
             #Use the patient's own ID
             current_patient = patients[0]
@@ -113,45 +116,51 @@ class Chatbot:
             source = 'All information kommer från din journal.'
             explanation = 'Assistenten utgår alltid från informationen som finns tillgänglig i din egen journal.'
 
-        #Get patient data related to the query
+        
+        # Get the system message!
+        system_message = self.get_system_message()
+
+
+        # Language level string to be added to the system message
+        language_level_sys_message = self.get_settings_system_message(settings)
+
+        # Replace unnecessary line with language level setting
+        system_message = system_message.replace('Du ska svara på följande meddelande "{{input}}".', language_level_sys_message)
+
+
+        # Get patient data related to the query
         patient_data = ''
         if current_patient != '' and current_patient != None:
             patient_data = query_db(query=query,id=current_patient,name='patientrecords')
             patient_data = ' '.join(patient_data['documents'][0])
 
-        
+        # Update the system message with relevant patient information
+        system_message = system_message.replace('background', patient_data)
 
-        #Update the system message with information depending on user role
-        if self.user_type == 'doctor':
-            sys_message = f'''{sys_message_doctor} \n ___{patient_data}___'''
-        elif self.user_type == 'patient':
-            sys_message = f'''{sys_message_patient} \n ___{patient_data}___'''
+        if remember:
+            messages = self.memory
+        else:
+            # Reset memory
+            messages = [{'role':'system', 'content': system_message}]
 
-        #Get the current memory
-        messages = self.memory
+        # Update the system message with relevant information for every question
+        messages[0] = {'role':'system', 'content': system_message}
 
-        if not remember:
-            #Reset conversation memory
-            messages = [{'role':'system', 'content': sys_message}]
-
-        #Update the system message with relevant information for every question
-        messages[0] = {'role':'system', 'content': sys_message}
-
-        #Add the query to the conversation memory
+        # Add the query to the conversation memory
         messages.append({'role':'user','content':query})
 
-        #Get a response from the model
+        # Get a response from the model
         response = openai.ChatCompletion.create(
             model=model,
             messages=messages,
             temperature=0, #Degree of randomness of the model's output
         )
 
-        #Add the response to the conversation memory
+        # Add the response to the conversation memory
         messages.append({'role':'assistant','content':response.choices[0].message['content']})
 
         
-        #Set the explanation for the response
+        # Set the explanation for the response
         #TODO explanation = thought_process
 
         final_response = response.choices[0].message['content']

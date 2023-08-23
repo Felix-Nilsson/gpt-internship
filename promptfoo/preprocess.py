e821820e1b3cf1f383e52bebbfd5d6a26645d714
OGPauli <oskar.pauli@bredband.net>
2023-08-23 15:52:54 +0200

    :speech_balloon: started commenting code for preprocessing

--- promptfoo/preprocess.py
+++ promptfoo/preprocess.py
@@ -9,6 +9,7 @@ from db.chroma import query_db_with_id
 
 
 def preprocess():
+    """Function that returns the relevant text chunks that should be used when answering the prompt. Used for evaluating prompt with promptfoo."""
     json_data = query_db_with_id(
         query="Vad är orsaken bakom hjärtinfarkten som patienten upplevde?",
         id="789012",
